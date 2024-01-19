e615c0f524b837fe9a2516ae07d738b736ad9d41
fere122 <06blake.krueger@gmail.com>
2024-01-19 08:32:33 -0500

    Square different RGB

--- Sandbox.js
+++ Sandbox.js
@@ -3,14 +3,13 @@
 //Program: Sphero Bolt Testing 
 //Bolt ID: 2fae
 
-/*
+/*)
 async function startProgram() {
 	await speak("Hello World", true);
 	setMainLed({ r: 0, g: 0, b: 255});
 	setSpeed(60);
 	await delay (2);
 	setSpeed(0);
-
 }
 */
 
@@ -27,4 +26,18 @@ async function startProgram() {
 
 
 
+ 
 // Figure 8
+async function startProgram() {
+	await roll(0, 50, 2)
+	await spin(90, 2.5)
+	setMainLed({ r: 0, g: 0, b: 255})
+	await roll(90, 50, 2)
+	await spin(90, 2.5)
+	setMainLed({ r: 0, g: 230, b: 255})
+	await roll(180, 50, 2)
+	await spin(90, 2.5)
+	setMainLed({ r: 100, g: 230, b: 0})
+ 	await roll(-90, 50, 2)
+	await Stop(0)
+}
