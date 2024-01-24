92d4305f845e152fbce1582d5ad42e25f49411f0
fere122 <06blake.krueger@gmail.com>
2024-01-24 08:09:10 -0500

    Maze Blue LED

--- Maze.js
+++ Maze.js
@@ -1,4 +1,17 @@
-// Team Members: Blake & Logan
+// Team Members: Blake K - Logan T
+// Name: Blake Krueger 
 // Date: 1.22.24
 // Program: Maze
+// Bolt: ID:2fae
 
+
+// Blue LED Checkpoint
+
+async function startProgram() {
+	await scrollMatrixText('Start', { r: 660, g: 560, b: 255 }, 15, true)
+	await speak("start")
+	await roll (-90, 55, 3.2)
+	setMainLed({ r: 0, g: 0, b: 90 })
+	
+	
+}
