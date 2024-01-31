3af4407832483c8117f372e713985e1d7f69c54b
fere122 <06blake.krueger@gmail.com>
2024-01-31 07:33:39 -0500

    Maze Green LED wip

--- Maze.js
+++ Maze.js
@@ -10,19 +10,20 @@
 async function startProgram() {
 	await scrollMatrixText('Start', { r: 660, g: 560, b: 255 }, 15, true)
 	await speak("start")
-	await roll (0 , 55, 2.7)
+	await roll (0 , 55, 2.5)
 	setMainLed({ r: 0, g: 0, b: 90 })
 	stopRoll(0)
-	await roll (90 , 51, 2.2)
+	await roll (90 , 50, 2.2)
 	await Sound.play()
-	await roll (180, 33, 2.5)
-	await roll(220, 28, 3.6,)
+	await roll (180, 33, 3)
+	await roll(220, 28, 2.6)
 	setMainLed({ r: 200, g: 0, b: 0 })
 	await roll (850, 22, 2.1)
-	await roll (400, 30, 2.3)
+	await roll (400, 30, 2.5)
 	await Sound.play()
-	await Sound.play()
-	await Sound.play()
+	await roll(500, 30, 2.5)
+	stopRoll(0)
+	
 	
 	
 
